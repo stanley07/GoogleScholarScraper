eda6fe034fb6de0c33f84260fd3b0336cfcf27ef
stanley07 <ezebo001@gmail.com>
2023-06-23 12:09:14 +0100

    update dependencies

--- env/lib/python3.10/site-packages/tzlocal/__init__.py
+++ env/lib/python3.10/site-packages/tzlocal/__init__.py
@@ -1,20 +1,5 @@
 import sys
-
-if sys.platform == "win32":
-    from tzlocal.win32 import (
-        get_localzone,
-        get_localzone_name,
-        reload_localzone,
-    )  # pragma: no cover
+if sys.platform == 'win32':
+    from tzlocal.win32 import get_localzone, reload_localzone
 else:
-    from tzlocal.unix import get_localzone, get_localzone_name, reload_localzone
-
-from tzlocal.utils import assert_tz_offset
-
-
-__all__ = [
-    "get_localzone",
-    "get_localzone_name",
-    "reload_localzone",
-    "assert_tz_offset",
-]
+    from tzlocal.unix import get_localzone, reload_localzone
